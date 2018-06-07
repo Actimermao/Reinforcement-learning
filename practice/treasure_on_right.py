5dea3931fec26ee4c4d7dfbe7993c88e3b22c8ff
Actimermao <853830387@qq.com>
2018-06-07 17:29:57 +0800

    Update treasure_on_right.py

--- practice/treasure_on_right.py
+++ practice/treasure_on_right.py
@@ -84,7 +84,7 @@ def rl():
             S = S_
             update_env(S, episode, step_counter+1)
             step_counter += 1
-            print('\n',q_table)
+            #print('\n',q_table)
     return q_table
 
 
